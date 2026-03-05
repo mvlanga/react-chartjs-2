3b269cfda7e3e5a4d9e14452990e6c94f2d646b0
Moriz von Langa <121946962+mvlanga@users.noreply.github.com>
2026-03-05 10:05:58 +0100

    fix: adjust grouped bar chart title

--- sandboxes/bar/grouped/App.tsx
+++ sandboxes/bar/grouped/App.tsx
@@ -23,7 +23,7 @@ export const options = {
   plugins: {
     title: {
       display: true,
-      text: 'Chart.js Bar Chart - Stacked',
+      text: 'Chart.js Bar Chart - Grouped',
     },
   },
   responsive: true,
